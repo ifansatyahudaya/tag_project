229f3cfbd3c458c86cdbc0bc73b208b703856d3c
ifan <ifansatyahudaya@gmail.com>
2016-09-28 09:58:20 +0700

    done fixing migration errors in heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914062230) do
+ActiveRecord::Schema.define(version: 20160928025351) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
