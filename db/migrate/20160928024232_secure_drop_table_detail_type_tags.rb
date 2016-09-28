d65fd6786d605d40f93c9618ab5411d9d49bf00c
ifan <ifansatyahudaya@gmail.com>
2016-09-28 09:46:37 +0700

    secure drop table detail_type_tags

--- db/migrate/20160928024232_secure_drop_table_detail_type_tags.rb
+++ db/migrate/20160928024232_secure_drop_table_detail_type_tags.rb
@@ -2,5 +2,6 @@ class SecureDropTableDetailTypeTags < ActiveRecord::Migration
   def change
     if ActiveRecord::Base.connection.table_exists? 'detail_type_tags'
       drop_table :detail_type_tags
+    end
   end
 end
